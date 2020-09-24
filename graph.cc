533f4999165406b6a68519d7b687d0759e77167f
Irek Szcześniak <irek.szczesniak@gmail.com>
2020-09-24 08:54:36 +0200

    a graph implementation

---
+++ graph.cc
@@ -0,0 +1,100 @@
+#include "graph.hpp"
+
+#include "utils.hpp"
+
+#include <iterator>
+#include <ostream>
+
+std::ostream &
+operator<<(std::ostream &out, const edge &e)
+{
+  out << "edge(" << name(source(e)) << " -> " << name(target(e))
+      << ", " << "cost = " << cost(e) << ", units = " << units(e)
+      << ")";
+
+  return out;
+}
+
+Index<vertex>
+index(const vertex &v)
+{
+  return v.m_index;
+}
+
+const std::string &
+name(const vertex &v)
+{
+  return v.m_name;
+}
+
+const std::vector<edge> &
+edges(const vertex &v)
+{
+  return v.m_edges;
+}
+
+const graph::vertexes_type &
+vertexes(const graph &g)
+{
+  return g.m_vertexes;
+}
+
+graph::size_type
+num_vertexes(const graph &g)
+{
+  return std::distance(g.m_vertexes.begin(), g.m_vertexes.end());
+}
+
+const vertex &
+source(const edge &e)
+{
+  return e.m_source;
+}
+
+const vertex &
+target(const edge &e)
+{
+  return e.m_target;
+}
+
+const edge::cost_type &
+cost(const edge &e)
+{
+  return e.m_cost;
+}
+
+const edge::units_type &
+units(const edge &e)
+{
+  return e.m_units;
+}
+
+const vertex::units_type &
+units(const vertex &v)
+{
+  return v.m_units;
+}
+
+vertex &
+add_vertex(graph &g, const std::string name,
+           vertex::units_type units)
+{
+  auto i = g.m_vertexes.size();
+  g.m_vertexes.push_back(vertex(i, name, units));
+  return g.m_vertexes.back();
+}
+
+void
+add_edge(graph &g, vertex &a, const vertex &b,
+         edge::cost_type cost, edge::units_type units)
+{
+  a.m_edges.push_back(edge(a, b, cost, units));
+}
+
+void
+add_edge_pair(graph &g, vertex &a, vertex &b,
+              edge::cost_type cost, edge::units_type units)
+{
+  a.m_edges.push_back(edge(a, b, cost, units));
+  b.m_edges.push_back(edge(b, a, cost, units));
+}
---
+++ graph.hpp
@@ -0,0 +1,240 @@
+#ifndef GRAPH_HPP
+#define GRAPH_HPP
+
+#include <list>
+#include <ostream>
+#include <set>
+#include <string>
+#include <tuple>
+#include <vector>
+
+// *******************************************************************
+// This file implements a directed graph.
+// *******************************************************************
+
+struct edge;
+
+struct vertex
+{
+  using units_type = std::set<unsigned>;
+  using edges_type = std::vector<edge>;
+  using index_type = edges_type::size_type;
+
+  // The index of the vertex.
+  index_type m_index;
+
+  // The name of the vertex.
+  std::string m_name;
+
+  // The units offered by this vertex.
+  units_type m_units;
+
+  vertex(index_type index, std::string name = {},
+         units_type units = {}):
+    m_index(index), m_name(name), m_units(units)
+  {
+  }
+
+  bool operator==(const vertex &b) const
+  {
+    return this == &b;
+  }
+
+  // We're using a vector, because we're frequently iterating over the
+  // edges of a vertex.  When using a vector, iterators or references
+  // to edges can invalidate when we add a new vertex, because the
+  // vector can be reallocated.  However, we don't mind that, because
+  // we don't store references to edges anywhere.
+  edges_type m_edges;
+};
+
+struct edge
+{
+  using cost_type = unsigned;
+  using units_type = std::set<unsigned>;
+
+  // The source node of the edge.
+  const vertex &m_source;
+  // The target node of the edge.
+  const vertex &m_target;
+
+  // The cost of an edge.
+  cost_type m_cost;
+
+  // The units required to take the edge.
+  units_type m_units;
+
+  edge(const vertex &source, const vertex &target, cost_type cost,
+       units_type units = {}):
+    m_source(source), m_target(target), m_cost(cost), m_units(units)
+  {
+  }
+
+  // We need this operator to establish order between edges.  This is
+  // needed by the generic_label which defines order between labels.
+  bool operator<(const edge &b) const
+  {
+    return this < &b;
+  }
+
+  bool operator==(const edge &b) const
+  {
+    return std::tie(m_source, m_target, m_cost, m_units) ==
+      std::tie(b.m_source, b.m_target, b.m_cost, b.m_units);
+  }
+};
+
+std::ostream &
+operator<<(std::ostream &, const edge &e);
+
+// The graph.
+struct graph
+{
+  using size_type = unsigned;
+  using vertexes_type = std::list<vertex>;
+
+  graph() = default;
+
+  // Can't copy, because edges have references to vertexes.  I would
+  // have to implement this.  For now, I'm disabling copying.
+  graph(const graph &) = delete;
+
+  // Can move.
+  graph(graph &&) = default;
+  
+  // We need a container which does not invalidate references (or
+  // iterators) to its elements when new elements are added (something
+  // a vector would do).  We have to make sure that references are not
+  // invalidated, because they are stored in edges.  Perhaps we could
+  // use std::forward_list instead of std::list, but we would have a
+  // problem with calculating the index for an node being added.
+  vertexes_type m_vertexes;
+};
+
+// *******************************************************************
+// The vertex traits
+
+template<typename>
+struct vertex_traits;
+
+template<>
+struct vertex_traits<graph>
+{
+  using type = vertex;
+};
+
+template<>
+struct vertex_traits<edge>
+{
+  using type = vertex;
+};
+
+template <typename T>
+using Vertex = typename vertex_traits<T>::type;
+
+// *******************************************************************
+// The edge traits
+
+template<typename>
+struct edge_traits;
+
+template<>
+struct edge_traits<graph>
+{
+  using type = edge;
+};
+
+template <typename G>
+using Edge = typename edge_traits<G>::type;
+
+// *******************************************************************
+// The cost traits
+
+template <typename>
+struct cost_traits;
+
+template <>
+struct cost_traits<edge>
+{
+  using type = edge::cost_type;
+};
+
+template <typename T>
+using Cost = typename cost_traits<T>::type;
+
+// *******************************************************************
+// The index traits
+
+template <typename>
+struct index_traits;
+
+template <>
+struct index_traits<vertex>
+{
+  using type = vertex::index_type;
+};
+
+template <typename T>
+using Index = typename index_traits<T>::type;
+
+// *******************************************************************
+// The units traits
+
+template <typename>
+struct units_traits;
+
+template <>
+struct units_traits<edge>
+{
+  using type = edge::units_type;
+};
+
+template <typename T>
+using Units = typename units_traits<T>::type;
+
+// *******************************************************************
+// The functions.
+
+Index<vertex>
+index(const vertex &);
+
+const std::string &
+name(const vertex &);
+
+const std::vector<edge> &
+edges(const vertex &);
+
+const graph::vertexes_type &
+vertexes(const graph &);
+
+graph::size_type
+num_vertexes(const graph &);
+
+const vertex &
+source(const edge &);
+
+const vertex &
+target(const edge &);
+
+const edge::cost_type &
+cost(const edge &);
+
+const edge::units_type &
+units(const edge &);
+
+const vertex::units_type &
+units(const vertex &);
+
+vertex &
+add_vertex(graph &g, const std::string name,
+           vertex::units_type units = {});
+
+void
+add_edge(graph &g, vertex &, const vertex &, edge::cost_type,
+         edge::units_type = {});
+
+void
+add_edge_pair(graph &g, vertex &, vertex &,
+              edge::cost_type, edge::units_type = {});
+
+#endif // GRAPH_HPP
